0f851e879dd3352f292cf1a92aaa1c1e56bfdc71
Cofredigital <digitalcofre@hotmail.com>
2026-01-14 14:50:37 +0000

    Setup Next.js project and Firebase lib

---
+++ cofre-digital-web/lib/firebase.ts
@@ -0,0 +1,23 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "...",
+  authDomain: "...",
+  projectId: "...",
+  storageBucket: "...",
+  messagingSenderId: "...",
+  appId: "..."
+};
+
+
+
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+
+export default app;
